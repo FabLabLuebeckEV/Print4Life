411e47fa00cfa1f6185c0f7b215f3b858100f802
Rene Kremer <renejon90@googlemail.com>
2018-07-23 14:21:05 +0200

    * update method to backup and restore database

--- db-seeds/scripts/create-seeds.sh
+++
@@ -1,6 +0,0 @@
-#!bin/sh
-
-echo "exporting from $1 collection: $2"
-mongo ${1} --eval "db.${2}.find({}, {_id:0, __v:0}).forEach(printjson);" > ${2}-export.json
-node jsonify.js ${2}-export.json > ../${2}.json
-rm ${2}-export.json
--- db-seeds/scripts/export.sh
+++ db-seeds/scripts/export.sh
@@ -1,17 +1,5 @@
 #!bin/sh
 
-# example (exectued in folder 'db-seeds/scripts'): bash export.sh localhost:27017/iot-fablab-dev
+# example (exectued in folder 'db-seeds'): bash scripts/export.sh localhost 27017 iot-fablab-dev
 
-bash create-seeds.sh $1 fablabs
-
-bash create-seeds.sh $1 lasercutterlasertypes
-
-bash create-seeds.sh $1 lasercutters
-
-bash create-seeds.sh $1 materials
-
-bash create-seeds.sh $1 millingmachines
-
-bash create-seeds.sh $1 othermachines
-
-bash create-seeds.sh $1 printers
+mongodump -h $1 -p $2 -d $3
