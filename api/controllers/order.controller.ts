98b708f03911ade5075bea80532edba2a80dabda
Immo Traupe <traupe@itm.uni-luebeck.de>
2018-07-25 11:37:36 +0200

    wrote API Doc for order controller #1

--- api/controllers/order.controller.ts
+++ api/controllers/order.controller.ts
@@ -6,7 +6,7 @@ import orderSchema from '../models/order.model';
 const Order = mongoose.model('Order', orderSchema);
 
 /**
- * @api {get} /api/v1/order Request the list of orders
+ * @api {get} /api/v1/orders/ Request the list of orders
  * @apiName GetOrders
  * @apiVersion 0.0.1
  * @apiGroup Orders
@@ -47,22 +47,168 @@ function getOrders () {
   });
 }
 
+/**
+ * @api {get} /api/v1/orders/:id Request an order by its id
+ * @apiName getOrderById
+ * @apiVersion 0.0.1
+ * @apiGroup Orders
+ *
+ * @apiSuccess { order } a single order
+ *
+ * @apiSuccessExample Success-Response:
+ *    HTTP/1.1 200 OK
+ *  {
+    "order": {
+        "status": "new",
+        "_id": "5b55cf9730b4aa4bbeaf6f68",
+        "comments": [
+            {
+                "_id": "5b583da5514fac1bb10832b6",
+                "author": "Mister Foo",
+                "content": "Hello there, could you print this?"
+            }
+        ],
+        "editor": "Mister Bar",
+        "owner": "Mister X",
+        "files": [],
+        "token": "42",
+        "__v": 0
+    }
+}
+ */
 function getOrderById (id) {
   return Order.findOne({ _id: id });
 }
 
+/**
+ * @api {post} /api/v1/orders/placeOrder Adds a new order
+ * @apiName placeOrder
+ * @apiVersion 0.0.1
+ * @apiGroup Orders
+ *
+ * @apiSuccess { order } the new order object, if success
+ *
+ * @apiSuccessExample Success-Response:
+ *    HTTP/1.1 200 OK
+  {
+    "order": {
+        "status": "new",
+        "_id": "5b58401ee7e9a1240d4b09ce",
+        "comments": [
+            {
+                "_id": "5b58401ee7e9a1240d4b09cf",
+                "author": "Mister Foo",
+                "content": "Hello there, could you print this?"
+            }
+        ],
+        "editor": "Mister Bar",
+        "owner": "Mister X",
+        "files": [],
+        "token": "42",
+        "__v": 0
+    }
+  }
+ */
 function placeOrder (newOrder) {
-  return Order(newOrder).save();
+  return Order(rmDbVars(newOrder)).save();
 }
 
+/**
+ * @api {post} /api/v1/orders/updateOrder Updates an order
+ * @apiName updateOrder
+ * @apiVersion 0.0.1
+ * @apiGroup Orders
+ *
+ * @apiSuccess { order } the updated order
+ *
+ * @apiSuccessExample Success-Response:
+ *    HTTP/1.1 200 OK
+  {
+  'order'; : {
+      'status'; : 'new',
+      '_id'; : '5b55cf9730b4aa4bbeaf6f68',
+      'comments'; : [
+          {
+              '_id': '5b583da5514fac1bb10832b6',
+              'author': 'Mister Foo',
+              'content': 'Hello there, could you print this?'
+          }
+      ],
+      'editor'; : 'Mister Bar',
+      'owner'; : 'Mister X',
+      'files'; : [],
+      'token'; : '42',
+      '__v'; : 0;
+  }
+ * @apiErrorExample {json} Error-Response:
+ *     HTTP/1.1 400 Bad Request
+  {
+      "error": "Order not found or malformed update.",
+      "stack": {
+          "message": "Cast to ObjectId failed for value \"BADID\" at path \"_id\" for model \"Order\"",
+          "name": "CastError",
+          "stringValue": "\"BADID\"",
+          "kind": "ObjectId",
+          "value": "BADID",
+          "path": "_id"
+      }
+  }
+ */
 function updateOrder (order) {
   delete order.__v;
   return Order.update({ _id: order._id }, order, { upsert: true }).then(() => Order.findOne({ _id: order._id }));
 }
 
+/**
+ * @api {post} /api/v1/orders/deleteOrder Marks an order as deleted
+ * @apiName delteOrder
+ * @apiVersion 0.0.1
+ * @apiGroup Orders
+ *
+ * @apiSuccess { order } the deleted order
+ *
+ * @apiSuccessExample Success-Response:
+ *    HTTP/1.1 200 OK
+  {
+  'order'; : {
+      'status'; : 'deleted',
+      '_id'; : '5b55cf9730b4aa4bbeaf6f68',
+      'comments'; : [
+          {
+              '_id': '5b583da5514fac1bb10832b6',
+              'author': 'Mister Foo',
+              'content': 'Hello there, could you print this?'
+          }
+      ],
+      'editor'; : 'Mister Bar',
+      'owner'; : 'Mister X',
+      'files'; : [],
+      'token'; : '42',
+      '__v'; : 0;
+  }
+ * @apiErrorExample {json} Error-Response:
+ *     HTTP/1.1 400 Bad Request
+  {
+      "error": "Order not found.",
+      "stack": {
+          "message": "Cast to ObjectId failed for value \"BADID\" at path \"_id\" for model \"Order\"",
+          "name": "CastError",
+          "stringValue": "\"BADID\"",
+          "kind": "ObjectId",
+          "value": "BADID",
+          "path": "_id"
+      }
+  }
+ */
 function deleteOrder (order) {
-  order['status'] = 'deleted';
+  order.status = 'deleted';
   return updateOrder(order);
+}
+
+function rmDbVars (obj) {
+  delete obj.__v;
+  delete obj._id;
+  return obj;
 }
 
 export default {
