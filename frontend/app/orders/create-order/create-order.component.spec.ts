f924b54918403bf44efa60b43498f658e0692e06
Immo Traupe <traupe@itm.uni-luebeck.de>
2018-07-30 15:17:46 +0200

    fixed frontend spec
    #1

--- frontend/app/orders/create-order/create-order.component.spec.ts
+++ frontend/app/orders/create-order/create-order.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
 import { CreateOrderComponent } from './create-order.component';
+
+import {
+  HttpClientTestingModule
+} from '@angular/common/http/testing';
 
 describe('CreateOrderComponent', () => {
   let component: CreateOrderComponent;
@@ -8,7 +13,8 @@ describe('CreateOrderComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CreateOrderComponent ]
+      declarations: [ CreateOrderComponent ],
+      imports: [RouterTestingModule, FormsModule, HttpClientTestingModule]
     })
     .compileComponents();
   }));
