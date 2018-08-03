7b8e2738569633d6b6a9ff1582077515c42da5ad
Rene Kremer <renejon90@googlemail.com>
2018-08-03 16:33:08 +0200

    * fix machine detail view spec

--- frontend/app/machines/machine-detail/machine-detail.component.spec.ts
+++ frontend/app/machines/machine-detail/machine-detail.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MachineDetailComponent } from './machine-detail.component';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import {
+  HttpClientTestingModule
+} from '@angular/common/http/testing';
 
 describe('MachineDetailComponent', () => {
   let component: MachineDetailComponent;
@@ -8,7 +13,9 @@ describe('MachineDetailComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MachineDetailComponent ]
+      declarations: [ MachineDetailComponent ],
+      imports: [NgbModule.forRoot(), HttpClientTestingModule,
+        RouterTestingModule]
     })
     .compileComponents();
   }));
