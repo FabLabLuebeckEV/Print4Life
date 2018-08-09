6016d3b5bce314902dbd5a8b204d03cd5976d62f
Immo Traupe <traupe@itm.uni-luebeck.de>
2018-08-09 14:10:26 +0200

    OrderDetail completed
    #30

--- frontend/app/orders/order-detail/order-detail.component.ts
+++ frontend/app/orders/order-detail/order-detail.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfigService } from '../../config/config.service';
+import { routes } from '../../config/routes';
+import { ActivatedRoute } from '@angular/router';
+import { OrderService } from '../../services/order.service';
+import { Order, Comment } from '../../models/order.model';
 
 @Component({
   selector: 'app-order-detail',
@@ -6,10 +11,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-detail.component.css']
 })
 export class OrderDetailComponent implements OnInit {
+  private config: any;
+  backArrow: any;
+  backLink: any;
+  order: Order = new Order(undefined, undefined, [], undefined, undefined, [], undefined, undefined);
+  orderPops: Object = {
+    title: 'Order Details',
+    props: []
+  };
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private orderService: OrderService,
+    private configService: ConfigService
+  ) {
+    this.config = this.configService.getConfig();
+    this.backArrow = this.config.icons.back;
+    this.backLink = `/${routes.paths.frontend.orders.root}`;
+    this.route.params.subscribe(params => {
+      if (params.id) {
+        this.orderService.getOrderById(params.id).then((result) => {
+          this.order = result.order;
+        });
+      }
+    });
+  }
 
   ngOnInit() {
   }
-
 }
