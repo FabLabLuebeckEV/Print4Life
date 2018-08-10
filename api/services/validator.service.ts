c17f3b363198453e7e68d960b910c40601c56cc4
Rene Kremer <renejon90@googlemail.com>
2018-08-10 15:19:17 +0200

    * use mongoose.Types.ObjectId.isValid() to ensure valid ids in requests

--- api/services/validator.service.ts
+++ api/services/validator.service.ts
@@ -1,7 +1,10 @@
+import * as mongoose from 'mongoose';
+
 function checkId (id) {
   let retObj;
-  if (id.length !== 24) {
-    retObj = { status: 400, error: 'Id needs to be a 24 character long hex string!' };
+  const valid = mongoose.Types.ObjectId.isValid(id);
+  if (!valid) {
+    retObj = { status: 400, error: 'Malformed Id! Check if id is a 24 character long hex string!' };
   } else {
     retObj = undefined;
   }
