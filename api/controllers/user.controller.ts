827a9fc2cedac20549a4cf45a7f9ad14141b70b5
Immo Traupe <traupe@itm.uni-luebeck.de>
2018-08-22 11:09:57 +0200

    added api docs for getRoles

--- api/controllers/user.controller.ts
+++ api/controllers/user.controller.ts
@@ -1,7 +1,7 @@
 import { User } from '../models/user.model';
 import { Role, roleSchema } from '../models/role.model';
 
-async function signUp (user) {
+async function signUp(user) {
   delete user._id;
   delete user.__v;
   const newUser = new User({
@@ -12,7 +12,39 @@ async function signUp (user) {
   return newUser.save();
 }
 
-async function getRoles () {
+/**
+ * @api {get} /api/v1/users/roles Request all valid status
+ * @apiName getRoles
+ * @apiVersion 1.0.0
+ * @apiGroup Users
+ * @apiHeader (Needed Request Headers) {String} Content-Type application/json
+ *
+ * @apiSuccess { roles } a list of valid roles
+ *
+ * @apiSuccessExample Success-Response:
+ *    HTTP/1.1 204 No-Content
+ *
+ * @apiSuccessExample Success-Response:
+ *    HTTP/1.1 200 OK
+ *  {
+      "roles": [
+          "guest",
+          "user",
+          "editor",
+          "admin"
+      ]
+    }
+*
+* @apiErrorExample {json} Error-Response:
+ *     HTTP/1.1 500 Server Error
+  {
+      "error": "Error while trying to get all valid roles!",
+      "stack": {
+          ...
+      }
+  }
+ */
+async function getRoles() {
   return new Promise((resolve, reject) => {
     const roles = roleSchema.paths.role.enumValues;
     if (roles === undefined) {
