787cae109a24ac558fb3fda0e9cd9268588f200e
Immo Traupe <traupe@itm.uni-luebeck.de>
2018-08-22 11:19:54 +0200

    apiDocs for signup user added

--- api/controllers/user.controller.ts
+++ api/controllers/user.controller.ts
@@ -1,7 +1,47 @@
 import { User } from '../models/user.model';
 import { Role, roleSchema } from '../models/role.model';
 
-async function signUp(user) {
+/**
+ * @api {post} /api/v1/users/ Adds a new order
+ * @apiName createUser
+ * @apiVersion 1.0.0
+ * @apiGroup Users
+ * @apiHeader (Needed Request Headers) {String} Content-Type application/json
+ *
+ * @apiSuccess { user } the new user object, if success
+ *
+ * @apiSuccessExample Success-Response:
+ *    HTTP/1.1 201 Created
+  {
+    "user": {
+        "_id": "5b7d29ed40ddae62a94e5940",
+        "firstname": "Hans",
+        "lastname": "Der Tester",
+        "username": "Hansiii",
+        "password": "$2b$10$WDCFfQTo1KKLt3ZXtlkvuuso1Pxqu6FbkG6KvJWzchz2k0xeDNVYe",
+        "email": "hansiii@alm.de",
+        "address": {
+            "street": "Middlehofer Straße 42",
+            "zipCode": "421337",
+            "city": "Geldhausen",
+            "country": "Luxemburg"
+        },
+        "role": {
+            "role": "admin"
+        },
+        "__v": 0
+    }
+  }
+ * @apiErrorExample {json} Error-Response:
+ *     HTTP/1.1 400 Malformed Request
+  {
+      "error": "Malformed user, one or more parameters wrong or missing",
+      "stack": {
+          ...
+      }
+  }
+ */
+async function signUp (user) {
   delete user._id;
   delete user.__v;
   const newUser = new User({
@@ -44,7 +84,7 @@ async function signUp(user) {
       }
   }
  */
-async function getRoles() {
+async function getRoles () {
   return new Promise((resolve, reject) => {
     const roles = roleSchema.paths.role.enumValues;
     if (roles === undefined) {
