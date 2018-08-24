087b59bec7351e58a0d167718846353f6395130c
Rene Kremer <renejon90@googlemail.com>
2018-08-24 12:11:22 +0200

    * add version route to get current version of the software * add footer to app to show current version

--- frontend/app/app.component.ts
+++ frontend/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { routes } from './config/routes';
 
 @Component({
     selector: 'app-root',
@@ -7,10 +9,24 @@ import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    constructor(private translateService: TranslateService) {
+    version: String;
+    softwareName: String;
+    constructor(private translateService: TranslateService, private http: HttpClient) {
+        const promise = this.http.get(`${routes.backendUrl}/version`).toPromise();
+        promise.then((result: any) => {
+            this.version = result.version;
+        });
         this.translateService.setDefaultLang('en');
+        this._translate();
         this.translateService.onLangChange.subscribe((event: LangChangeEvent) => {
             this.translateService.use(event.lang);
+            this._translate();
+        });
+    }
+
+    private _translate() {
+        this.translateService.get(['navigation']).subscribe((translations) => {
+            this.softwareName = translations['navigation'].title;
         });
     }
 }
