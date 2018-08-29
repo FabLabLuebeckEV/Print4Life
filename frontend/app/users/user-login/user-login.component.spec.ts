0b7550684af272f284978802b7863bab1a25ce09
Immo Traupe <traupe@itm.uni-luebeck.de>
2018-08-29 15:44:30 +0200

    frontend spec for login fixed

--- frontend/app/users/user-login/user-login.component.spec.ts
+++ frontend/app/users/user-login/user-login.component.spec.ts
@@ -1,16 +1,25 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserLoginComponent } from './user-login.component';
-
+import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import {
+  HttpClientTestingModule
+} from '@angular/common/http/testing';
 describe('UserLoginComponent', () => {
   let component: UserLoginComponent;
   let fixture: ComponentFixture<UserLoginComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserLoginComponent ]
+      declarations: [UserLoginComponent],
+      imports: [
+        FormsModule,
+        RouterTestingModule,
+        HttpClientTestingModule
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
