5a752ea23dcf6060817317870d3481ef8d1c056b
Rene Kremer <renejon90@googlemail.com>
2018-08-30 14:38:57 +0200

    * fix order detail component spec
    #63

--- frontend/app/orders/order-detail/order-detail.component.spec.ts
+++ frontend/app/orders/order-detail/order-detail.component.spec.ts
@@ -9,6 +9,7 @@ import { OrderDetailComponent } from './order-detail.component';
 import { BackButtonComponent } from '../../components/back-button/back-button.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 describe('OrderDetailComponent', () => {
   let component: OrderDetailComponent;
@@ -19,11 +20,13 @@ describe('OrderDetailComponent', () => {
       declarations: [
         OrderDetailComponent, BackButtonComponent
       ],
+      providers: [TranslateService],
       imports: [
         NgbModule.forRoot(),
         HttpClientTestingModule,
         RouterTestingModule,
-        FontAwesomeModule
+        FontAwesomeModule,
+        TranslateModule.forRoot()
       ]
     })
       .compileComponents();
