c8eb482b014aa2855ec1862b02f288902fd16104
Rene Kremer <renejon90@googlemail.com>
2018-08-31 12:46:44 +0200

    * add todo for test specs once login is completed
    #51

--- api/specs/global.spec.ts
+++ api/specs/global.spec.ts
@@ -1,4 +1,5 @@
 'use strict';
+import { User } from '../models/user.model';
 
 const Jasmine = require('jasmine');
 const server = require('../index');
@@ -6,9 +7,13 @@ const server = require('../index');
 const jasmineLib = new Jasmine();
 
 server.run();
+// TODO: Create TestUser and use jwt for test specs as mongoose can be used here
+// User.find({}).then((result) => {
 jasmineLib.loadConfig({
   spec_dir: 'dist/specs',
   spec_files: ['**/*[sS]pec.js']
 });
 
 jasmineLib.execute();
+// });
+
