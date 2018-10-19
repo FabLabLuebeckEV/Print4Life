b4dad3e3184adc46da61f12b849907df8b26301a
Rene Kremer <renejon90@googlemail.com>
2018-10-19 10:00:25 +0200

    * fix specs

--- frontend/app/services/error.service.spec.ts
+++ frontend/app/services/error.service.spec.ts
@@ -2,6 +2,7 @@ import { TestBed, inject } from '@angular/core/testing';
 import { NgbModule, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { ErrorService } from './error.service';
 import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 import {
   HttpClientTestingModule
@@ -10,8 +11,8 @@ import {
 describe('ErrorService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [NgbModule.forRoot(), HttpClientTestingModule, RouterTestingModule],
-      providers: [NgbActiveModal, ErrorService]
+      imports: [NgbModule.forRoot(), HttpClientTestingModule, RouterTestingModule, TranslateModule.forRoot()],
+      providers: [NgbActiveModal, ErrorService, TranslateService]
     });
   });
 
