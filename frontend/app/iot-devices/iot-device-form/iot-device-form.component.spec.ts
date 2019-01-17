e1a69f3cd60c34b4535e12389d39c319af3aa64f
Immo Traupe <traupe@itm.uni-luebeck.de>
2019-01-17 15:46:02 +0100

    fxied specs

--- frontend/app/iot-devices/iot-device-form/iot-device-form.component.spec.ts
+++ frontend/app/iot-devices/iot-device-form/iot-device-form.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { IotDeviceFormComponent } from './iot-device-form.component';
+
+import {
+  HttpClientTestingModule
+} from '@angular/common/http/testing';
 
 describe('IotDeviceFormComponent', () => {
   let component: IotDeviceFormComponent;
@@ -8,9 +13,13 @@ describe('IotDeviceFormComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ IotDeviceFormComponent ]
+      declarations: [IotDeviceFormComponent],
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
