c12b00e10b6684f9c36afef7c509dbd1049786ee
Immo Traupe <traupe@itm.uni-luebeck.de>
2019-01-29 15:07:58 +0100

    added success and error modals

--- frontend/app/iot-devices/iot-device-form/iot-device-form.component.ts
+++ frontend/app/iot-devices/iot-device-form/iot-device-form.component.ts
@@ -9,6 +9,9 @@ import { User } from 'frontend/app/models/user.model';
 import { UserService } from 'frontend/app/services/user.service';
 import { FormBuilder, FormGroup, FormArray, FormControl } from '@angular/forms';
 import { Icon } from '@fortawesome/fontawesome-svg-core';
+import { ModalButton, MessageModalComponent } from 'frontend/app/components/message-modal/message-modal.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { GenericService } from 'frontend/app/services/generic.service';
 
 @Component({
   selector: 'app-iot-device-form',
@@ -40,6 +43,15 @@ export class IotDeviceFormComponent implements OnInit {
     messages: {
       deviceId: '',
       deviceId2: ''
+    },
+    modals: {
+      errorHeader: '',
+      successHeader: '',
+      ok: '',
+      username: '',
+      password: '',
+      dataformat: '',
+      saveHint: ''
     }
   };
   constructor(
@@ -49,7 +61,9 @@ export class IotDeviceFormComponent implements OnInit {
     private configService: ConfigService,
     private iotDeviceService: IotDeviceService,
     private userService: UserService,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private modalService: NgbModal,
+    private genericService: GenericService
   ) {
     this.config = this.configService.getConfig();
     this.plusIcon = this.config.icons.add;
@@ -88,17 +102,61 @@ export class IotDeviceFormComponent implements OnInit {
   }
 
   async onSubmit() {
+    const okButton = new ModalButton(this.translationFields.modals.ok, 'btn btn-primary', this.translationFields.modals.ok);
     let iotDeviceCopy;
     iotDeviceCopy = JSON.parse(JSON.stringify(this.iotDevice));
     iotDeviceCopy.deviceType = this.iotDevice.deviceType.id;
     iotDeviceCopy.events = this.eventFormGroup.value.events;
-    this.iotDeviceService.addDevice(iotDeviceCopy);
+    this.iotDeviceService.addDevice(iotDeviceCopy)
+      .then((result) => {
+        if (result && result['iot-device']) {
+          const device = result['iot-device'];
+          const message = [
+            `${this.translationFields.modals.saveHint}`,
+            `Client ID ${device.clientId}`,
+            `${this.translationFields.modals.username}: ${device.username}`,
+            `${this.translationFields.modals.password}: ${device.password}`
+          ];
+          device.events.forEach(event => {
+            message.push(`Event: Topic: ${event.topic}, ${this.translationFields.modals.dataformat}: ${event.dataformat}`);
+          });
+          this._openMsgModal(
+            this.translationFields.modals.successHeader,
+            'modal-header header-success', message, okButton, undefined).result.then((result) => {
+              this.genericService.back();
+            });
+        }
+      })
+      .catch(err => {
+        this._openMsgModal(
+          this.translationFields.modals.errorHeader,
+          'modal-header header-danger',
+          [this.translationFields.modals.errorHeader],
+          okButton,
+          undefined
+        );
+      });
   }
 
   private async _checkDeviceId(id: String) {
     let iotDevice;
     iotDevice = await this.iotDeviceService.getDeviceById(id);
     this.deviceIdAlreadyTaken = iotDevice ? true : false;
+    return iotDevice ? true : false;
+  }
+
+  private _openMsgModal(title: String, titleClass: String, messages: Array<String>,
+    button1: ModalButton, button2: ModalButton, link?: String) {
+    const modalRef = this.modalService.open(MessageModalComponent, { backdrop: 'static' });
+    modalRef.componentInstance.title = title;
+    modalRef.componentInstance.titleClass = titleClass;
+    modalRef.componentInstance.messages = messages;
+    modalRef.componentInstance.button1 = button1;
+    modalRef.componentInstance.button2 = button2;
+    if (link) {
+      modalRef.componentInstance.link = link;
+    }
+    return modalRef;
   }
 
   private async _loadDeviceTypes() {
@@ -126,6 +184,15 @@ export class IotDeviceFormComponent implements OnInit {
         messages: {
           deviceId: translations['iotDevicesForm'].messages.deviceId,
           deviceId2: translations['iotDevicesForm'].messages.deviceId2
+        },
+        modals: {
+          errorHeader: translations['iotDevicesForm'].modals.errorHeader,
+          ok: translations['iotDevicesForm'].modals.ok,
+          successHeader: translations['iotDevicesForm'].modals.successHeader,
+          username: translations['iotDevicesForm'].modals.username,
+          password: translations['iotDevicesForm'].modals.password,
+          dataformat: translations['iotDevicesForm'].modals.dataformat,
+          saveHint: translations['iotDevicesForm'].modals.saveHint
         }
       };
     }));
