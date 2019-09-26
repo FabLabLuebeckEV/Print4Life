e7bcb9874d6c5e0573ac4ef72e68ceb42f893019
Ian Pösse <poesseian@googlemail.com>
2019-09-26 09:14:40 +0200

    update order list for mobile devices

--- frontend/app/orders/order-list/order-list.component.ts
+++ frontend/app/orders/order-list/order-list.component.ts
@@ -72,7 +72,7 @@ export class OrderListComponent implements OnInit {
     page: 1,
     totalItems: 0,
     perPage: 20,
-    maxSize: 10,
+    maxSize: 3,
     boundaryLinks: true,
     rotate: true,
     maxPages: 0,
