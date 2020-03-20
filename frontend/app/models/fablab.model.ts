f1f57dafbecc9a02f01f4aa9fc9bdc8044d6d0ce
ZarJohn <zar@zartools.de>
2020-03-21 00:55:15 +0100

    Add Fablab Model + Service

---
+++ frontend/app/models/fablab.model.ts
@@ -0,0 +1,10 @@
+import { Address } from "./address.model";
+
+export class Fablab {
+  constructor(
+    public _id: String,
+    public name: String,
+    public address: Address,
+    public activated: Boolean
+  ) {}
+}
--- frontend/app/services/fablab.service.ts
+++ frontend/app/services/fablab.service.ts
@@ -1,15 +1,15 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { routes } from '../config/routes';
-
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { routes } from "../config/routes";
+import { Fablab } from "../models/fablab.model";
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class FablabService {
   private rootPath: String;
 
   constructor(private http: HttpClient) {
-    this.rootPath = routes.backendUrl + '/' + routes.paths.backend.fablabs.root;
+    this.rootPath = routes.backendUrl + "/" + routes.paths.backend.fablabs.root;
   }
 
   public getFablab(id): Promise<any> {
@@ -18,5 +18,11 @@ export class FablabService {
 
   public getFablabs(): Promise<any> {
     return this.http.get(`${this.rootPath}`).toPromise();
+  }
+  public createFablab(fablab: Fablab): Promise<any> {
+    return this.http.post(`${this.rootPath}/`, fablab).toPromise();
+  }
+  public updateFablab(fablab): Promise<any> {
+    return this.http.put(`${this.rootPath}/${fablab._id}`, fablab).toPromise();
   }
 }
