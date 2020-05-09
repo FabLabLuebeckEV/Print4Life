976bf7b7c8f282f2a4496b633ac95eabf6137181
Hannah <hannah@Administrators-MacBook-Pro.local>
2020-05-09 18:49:29 +0200

    Press page (still missing content)

--- frontend/app/press/press.component.ts
+++ frontend/app/press/press.component.ts
@@ -8,5 +8,17 @@ import { Component, OnInit } from '@angular/core';
 export class PressComponent implements OnInit {
     async ngOnInit() {
 
+
+        document.querySelectorAll('.toc a').forEach(el => {
+            el.addEventListener('click', ev => {
+                ev.preventDefault();
+
+                const selector = el.getAttribute('href');
+                const anchor = document.getElementById(selector.replace('#', ''));
+                if(anchor) {
+                    window.scroll({top: anchor.offsetTop - 50, left: 0, behavior: 'smooth'});
+                }
+            })
+        })
     }
 }
