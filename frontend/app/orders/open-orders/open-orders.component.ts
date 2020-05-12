e59b2f668296048ae95f0ef9ab87db9a62dcc72b
Ian Pösse <ian-github@ipoesse.de>
2020-05-12 23:30:58 +0200

    switch open-orders for hospitals to own open orders

--- frontend/app/orders/open-orders/open-orders.component.ts
+++ frontend/app/orders/open-orders/open-orders.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ElementRef, ViewChild, Input } from '@angular/core';
 
 import { OrderService } from '../../services/order.service';
 import { BlueprintService } from '../../services/blueprint.service';
+import { UserService } from 'frontend/app/services/user.service';
+import { User } from 'frontend/app/models/user.model';
 
 @Component({
     selector: 'app-open-orders',
@@ -11,8 +13,13 @@ import { BlueprintService } from '../../services/blueprint.service';
 export class OpenOrdersComponent implements OnInit {
 
     orders: Array<any>;
+    loggedInUser: User;
 
-    constructor(public orderService: OrderService, public blueprintService: BlueprintService) {
+    constructor(
+        public orderService: OrderService,
+        public blueprintService: BlueprintService,
+        public userService: UserService
+    ) {
 
     }
 
@@ -21,26 +28,39 @@ export class OpenOrdersComponent implements OnInit {
     }
 
     private async init() {
+        this.loggedInUser = await this.userService.findOwn();
         this.loadOrders();
     }
 
     public async  loadOrders() {
-        console.log('openorders loadOrders called');
-        const query = {
-            $and: [
-                {
-                    blueprintId: {
-                        $exists: true
-                    }
-                }
-            ]
-        };
+        const query = {$and: []};
+        query.$and.push({
+            blueprintId: {
+                $exists: true
+            }
+        });
+        if (this.loggedInUser.role.role === 'user') {
+            query.$and.push({
+                owner: this.loggedInUser._id
+            });
+        }
+
+
         const ret = await this.orderService.getAllOrders(query);
+
         for (let i = 0; i < ret.orders.length; i++) {
             ret.orders[i].blueprint = (await this.blueprintService.getBlueprint(ret.orders[i].blueprintId)).blueprint;
         }
-        this.orders = ret.orders;
 
-        console.log('orders: ', JSON.parse(JSON.stringify(this.orders)));
+        if (this.loggedInUser.role.role === 'user') {
+            ret.orders = ret.orders.filter(function (order) {
+                const finished = order.batch.finished.reduce((total, batch) => {
+                    return total + batch.number;
+                }, 0);
+                return order.batch.number > finished;
+            });
+        }
+
+        this.orders = ret.orders;
     }
 }
